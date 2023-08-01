52a4f51ce577b0c2099bde1e2a3f37a71a66fffe
colinhannigan <53630274+colinhannigan@users.noreply.github.com>
2023-08-01 14:08:55 -0500

    Add files via upload
    update Pollock Ch. 1 exercises

--- Pollock_Chapter1/initLesson.R
+++ Pollock_Chapter1/initLesson.R
@@ -3,21 +3,27 @@
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
 
-packages <- c("pacman","base64enc")
+# packages <- c("pacman","dplyr")
 
-for(p in packages)
-{
-  tryCatch(test <- require(p,character.only=T), 
-           warning=function(w) return())
-  if(!test)
-  {
-    print(paste("Package", p, "not found. Installing Package!"))
-    install.packages(p)
-    require(p)
-  }
-}
+
+
+# for(p in packages)
+# {
+#   print(paste("Yo, it's ",p))
+#   tryCatch(err<- require(p,character.only=T), 
+#            warning=function(w) return())
+#   if(!err)
+#   {
+#     print(paste("Package", p, "not found. Installing Package!"))
+#     install.packages(p)
+#     require(p)
+#   }
+# }
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(base64enc)
+
 
 swirl_options(swirl_logging = TRUE)
-
 library(base64enc)
 
